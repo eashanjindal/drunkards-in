cf5e5901bc35ad63568e655e4e3ec3921279aa05
macbook <macbook@192.168.1.4>
2026-04-19 01:16:31 +0530

    feat: cache busting + netlify cache headers

---
+++ public/js/main.995102d12e0635d2dff887a7c58ff78dfd36e5a21b50740a18aff9444a607588.js
@@ -0,0 +1,6 @@
+// Mobile nav toggle
+const toggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.main-nav');
+if (toggle && nav) {
+  toggle.addEventListener('click', () => nav.classList.toggle('open'));
+}
